c8614b56570cee5f2f59422acb4ce97c5d9131df
Brad Axe <bradaxelson@gmail.com>
2021-04-12 21:57:16 -0400

    refactor: Prefer observables

--- src/app/registries/module-registry.ts
+++ src/app/registries/module-registry.ts
@@ -1,16 +1,9 @@
 import { DynamicModulesMap } from '../tokens/dynamic-modules.token';
 
 export const ModuleRegistry: DynamicModulesMap = {
-  default: () =>
-    import('../components/default/default.module').then((m) => m.DefaultModule),
-  button: () =>
-    import('../components/button/button.module').then((m) => m.ButtonModule),
-  select: () =>
-    import('../components/select/select.module').then((m) => m.SelectModule),
-  form: () =>
-    import('../components/form/form.module').then((m) => m.DynamicFormModule),
-  formGroup: () =>
-    import('../components/form-group/form-group.module').then(
-      (m) => m.FormGroupModule
-    ),
+  default: () => import('../components/default/default.module'),
+  button: () => import('../components/button/button.module'),
+  select: () => import('../components/select/select.module'),
+  form: () => import('../components/form/form.module'),
+  formGroup: () => import('../components/form-group/form-group.module'),
 };
